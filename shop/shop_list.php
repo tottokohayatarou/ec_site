90869368948e6fc5504225e7f396a1b2ed5d3776
Ezura <ksw2070336@stu.o-hara.ac.jp>
2021-05-13 11:14:57 +0900

    更新

--- shop/shop_list.php
+++ shop/shop_list.php
@@ -1,6 +1,20 @@
 <?php session_start(); ?>
 <?php
     //データベース接続
+    $user = 'phpuser2';
+    $password = '1234';
+    $dbName = 'shop';
+    $host = 'localhost';
+    $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
+    try {
+        $pdo = new PDO($dsn, $user, $password);
+            $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (Exception $e) {
+            echo '<span class="エラーがありました。</span><br>';
+            echo $e->getMessage();
+            exit();
+        }
 ?>
 
 <!DOCTYPE html>
@@ -33,6 +47,7 @@
         ?>
     </header>
     <main>
+
     <?php
     $sql = 'SELECT * FROM dat_sales_product';
     $stm = $pdo->prepare($sql);
@@ -40,7 +55,13 @@
     $result = $stm->fetchAll(PDO::FETCH_ASSOC);
     foreach ($result as $row) {
         if(!$row['code']){
+    ?>
+        <a href="shop_list_type.php?id=<?= $row['code']?>">
+    <?php
             echo "{$row['name']}";
+    ?>
+        </a>
+    <?php
         }
     }
     ?>
